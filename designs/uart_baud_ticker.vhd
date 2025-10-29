72f1f2b619ba8701a66af28908d144ec7651d1b7
GabrielC248 <gabrielccoelho10@gmail.com>
2025-10-28 21:19:11 -0300

    Baud ticker feito!

---
+++ designs/uart_baud_ticker.vhd
@@ -0,0 +1,75 @@
+----------------------------------------------------------------------------------
+-- Company: CEPEDI
+-- Engineer: Gabriel Cavalcanti Coelho
+-- Create Date: 28.10.2025
+-- Module Name: uart_baud_ticker
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.math_real.all;
+
+entity uart_baud_ticker is
+    generic (
+        CLOCK          : natural := 25000000; -- Frequência do clock de entrada em Hz
+        BAUDRATE       : natural := 115200;   -- Baudrate desejado para a comunicação UART
+        OVERSAMPLE     : natural := 1;        -- Fator de oversampling
+        PHASE_FRACTION : real    := 0.5       -- Fator de fase fracionário (0.0 a 1.0) (0.5 para RX e 0.0 para TX)
+    );
+    port (
+        clk           : in  std_logic; -- Entrada do clock de sistema
+        n_rst         : in  std_logic; -- Reset síncrono, ativo em nível lógico baixo ('0')
+        phase_trigger : in  std_logic; -- Ao ativar, reseta o contador para o valor de fase
+        baud_tick     : out std_logic  -- Saída de um pulso ('tick'), ativa por um ciclo na frequência desejada
+    );
+end entity uart_baud_ticker;
+
+architecture rtl of uart_baud_ticker is
+
+    -- Constante que calcula o fator de divisão necessário
+    constant DIV : natural := natural(round(real(CLOCK)/real(BAUDRATE*OVERSAMPLE)));
+
+    -- Calcula o número de ciclos de atraso desejado a partir do trigger
+    constant DELAY_CYCLES : natural := natural(round(PHASE_FRACTION * real(DIV)));
+
+    -- Calcula o valor que deve ser carregado no contador para que
+    -- (DIV - PHASE_OFFSET_VALUE) seja o atraso desejado
+    constant PHASE_OFFSET_VALUE : natural := DIV - DELAY_CYCLES;
+
+    -- Sinal do contador que conta os ciclos do clock principal (clk)
+    signal counter_reg : natural range 0 to DIV-1 := 0;
+
+    -- Sinal que armazena o estado do pulso de saída
+    signal baud_tick_reg : std_logic := '0';
+
+begin
+
+    -- Processo síncrono que implementa a lógica de geração do pulso
+    counter_proc: process(clk)
+    begin
+        if rising_edge(clk) then
+            if (n_rst = '0') then -- Lógica de reset síncrono
+                counter_reg   <= 0;
+                baud_tick_reg <= '0';
+            else
+                if (phase_trigger = '1') then -- Lógica da fase 
+                    counter_reg   <= PHASE_OFFSET_VALUE;
+                    baud_tick_reg <= '0'; -- Garante que não haja tick durante o resync
+                else 
+                    if (counter_reg = DIV-1) then -- Lógica de contagem
+                        counter_reg   <= 0;
+                        baud_tick_reg <= '1';
+                    else -- Contagem normal
+                        counter_reg   <= counter_reg + 1;
+                        baud_tick_reg <= '0';
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process counter_proc;
+
+    -- Conecta o registrador à saída
+    baud_tick <= baud_tick_reg;
+
+end architecture rtl;
---
+++ testbenches/tb_uart_baud_ticker.vhd
@@ -0,0 +1,85 @@
+----------------------------------------------------------------------------------
+-- Company: CEPEDI
+-- Engineer: Gabriel Cavalcanti Coelho
+-- Create Date: 28.10.2025
+-- Module Name: tb_uart_baud_ticker
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity tb_uart_baud_ticker is
+end entity tb_uart_baud_ticker;
+
+architecture behavior of tb_uart_baud_ticker is
+
+    -- Período de clock para o testbench (500 MHz)
+    constant T : time := 2 ns;
+
+    -- Parâmetros
+    constant CLOCK_FREQ     : positive := 500000000; -- 500 MHz
+    constant OVERSAMPLE     : positive := 1;
+    constant BAUDRATE       : positive := 10000000;  -- 10 MHz
+    constant PHASE_FRACTION : real     := 0.5;
+    
+    -- Sinais de interface
+    signal clk           : std_logic := '0';
+    signal n_rst         : std_logic;
+    signal phase_trigger : std_logic;
+    signal baud_tick     : std_logic;
+
+begin
+
+    -- Instanciação do Módulo
+    UUT: entity work.uart_baud_ticker
+        generic map (
+            CLOCK          => CLOCK_FREQ,
+            BAUDRATE       => BAUDRATE,
+            OVERSAMPLE     => OVERSAMPLE,
+            PHASE_FRACTION => PHASE_FRACTION
+        )
+        port map (
+            clk           => clk,
+            n_rst         => n_rst,
+            phase_trigger => phase_trigger,
+            baud_tick     => baud_tick
+        );
+
+    -- Geração de Clock
+    clk_gen: process
+    begin
+        wait for T/2;
+        clk <= not clk;
+    end process clk_gen;
+
+    -- Processo de Estímulo
+    stimulus_proc: process
+    begin
+        -- Reset Inicial
+        n_rst         <= '0';
+        phase_trigger <= '0';
+        
+        -- Mantém em reset por 4 ciclos de clock
+        for i in 1 to 4 loop
+            wait until rising_edge(clk);
+        end loop;
+
+        -- Libera o reset
+        n_rst <= '1';
+
+        -- Espera pelos dois primeiros ticks
+        for i in 1 to 108 loop
+            wait until falling_edge(clk);
+        end loop;
+
+        -- Testa o phase_trigger
+        phase_trigger <= '1';
+        wait until rising_edge(clk);
+        phase_trigger <= '0';
+
+        wait;
+
+    end process stimulus_proc;
+
+end architecture behavior;
